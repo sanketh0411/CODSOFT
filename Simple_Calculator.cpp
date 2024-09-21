1d61d3055ddbc9b5748f252507e6c7484407aeee
Sanketh0411 <vishwakarmasuman237@gmail.com>
2024-09-21 10:20:10 +0530

    Add new file

--- Simple_Calculator.cpp
+++ Simple_Calculator.cpp
@@ -2,5 +2,38 @@
 using namespace std;
 int main()
 {
-    return 0
+    double a ,b;
+    char operation;
+    cout<<"====== SIMPLE CALCULATOR ====="<<endl;
+    cout<<" Enter The First Number : ";
+    cin>>a;
+    cout<<"Enter The Second Number : ";
+    cin>>b;
+    cout<<"Select The Operation (+,-,*,/) : ";
+    cin>>operation;
+    switch (operation)
+    {
+    case '+':
+        cout<<"Result : "<<a+b<<endl;
+        break;
+    case '-':
+        cout<<"Result : "<<a-b<<endl;
+        break;
+    case '*':
+        cout<<"Result : "<<a*b<<endl;
+        break;
+    case '/':
+        if (b==0)
+        {
+        cout<<"Divison With Zero is Not Allowed."<<endl;
+        } 
+    else
+    cout<<"Result : "<<a/b<<endl;
+    
+        break;
+
+    default:
+        cout<<"Invalid Operation.Please Enter Valid Operation."<<endl;   
+    }
+    return 0;
 }
